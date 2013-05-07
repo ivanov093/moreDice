0363202fe59f6243adb737b61080bd4837d03da9
ivanov093 <augustusIII@live.com>
2013-05-06 21:07:16 -0700

    Update and rename diceTest.txt to diceTest.java

---
+++ diceTest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class diceTest
+{
+  public static void main ( String[] args )
+  {
+  	Dice myCount = new Dice(0, 0);
+  	
+  	
+  	// Static
+  	int rollStatic = Dice.RollDice(3, 6);
+  	
+  	int numStaticCount = Dice.RollCount();
+  	
+  	System.out.printf("\nStatic Roll: %d\nStatic Cout: %d\n", rollStatic, numStaticCount);
+  	
+  	// Public 
+  	
+  	int numRoll = myCount.RollDice();
+  	int numRoll2 = myCount.RollDice();
+  	int numPublicCount = myCount.RollCountPublic();
+  	
+  	System.out.printf("\n1 Public Roll: %d\n2 Public Roll: %d\nPublic Count: %d\n", 
+  						numRoll, numRoll2, numPublicCount);
+  	
+  }
+}
